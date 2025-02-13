058b8034606a2dfc37da950908f660ecfcfec1f1
mdhruvil <dhruvil1808@gmail.com>
2025-02-13 19:03:22 +0530

    feat: rename repository scope from @repo to @deplit and add framework detector package

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { nextJsConfig } from "@deplit/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig;
