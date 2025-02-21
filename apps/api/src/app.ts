af47cdf8e193dfb60b07b451459a3376db42b997
mdhruvil <dhruvil1808@gmail.com>
2025-02-21 16:37:44 +0530

    feat(api): test azure logs

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,5 +2,8 @@ import { Hono } from "hono";
 import { env } from "./env.js";
 
 export const app = new Hono({ strict: false }).get("/", (c) => {
+  c.req.raw.headers.forEach((value, key) => {
+    console.log(`${key}: ${value}`);
+  });
   return c.json({ message: "OK!", env });
 });
