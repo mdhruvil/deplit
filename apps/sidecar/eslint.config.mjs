75fd316b28bc84e983dc91cb92b587790d8b4040
mdhruvil <dhruvil1808@gmail.com>
2025-03-12 21:12:42 +0530

    feat(sidecar): init

--- apps/sidecar/eslint.config.mjs
+++ apps/sidecar/eslint.config.mjs
@@ -3,5 +3,9 @@ import { config } from "@deplit/eslint-config/base";
 /** @type {import("eslint").Linter.Config} */
 export default [
   ...config,
-  { rules: { "turbo/no-undeclared-env-vars": "off" } },
+  {
+    rules: {
+      "turbo/no-undeclared-env-vars": "off",
+    },
+  },
 ];
