9d789478e07130253388d1aedc5ad6331c50124b
mdhruvil <dhruvil1808@gmail.com>
2025-04-01 17:07:58 +0530

    feat(sidecar): implement token based auth between sidecar and builder to only allow our code to invoke the sidecar api

--- apps/sidecar/src/index.ts
+++ apps/sidecar/src/index.ts
@@ -1,11 +1,27 @@
 import { serve } from "@hono/node-server";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { bearerAuth } from "hono/bearer-auth";
+import { ingestLogsSchema, updateBuildStatusSchema } from "./validators.js";
 
-const app = new Hono();
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+const app = new Hono()
+  .use("*", bearerAuth({ token: process.env.INTERNAL_API_TOKEN! }))
+  .post("/logs/ingest", zValidator("json", ingestLogsSchema), async (c) => {
+    const body = c.req.valid("json");
+    console.log(
+      `${body.timestamp.toISOString()} [${body.level.toUpperCase()}]: ${body.message}`,
+    );
+    return c.text("Logs received");
+  })
+  .post(
+    "/build-status",
+    zValidator("json", updateBuildStatusSchema),
+    async (c) => {
+      const { status, message } = c.req.valid("json");
+      console.log("Build status updated:", status, message);
+      return c.text("Build status updated");
+    },
+  );
 
 serve(
   {
