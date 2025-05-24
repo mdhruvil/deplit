8d30d2e5e7ceb117b4b05a55e42206cafd585652
mdhruvil <dhruvil1808@gmail.com>
2025-05-24 17:03:32 +0530

    fix(api): hardcode ph token

--- apps/api/src/lib/posthog.ts
+++ apps/api/src/lib/posthog.ts
@@ -1,10 +1,10 @@
-import { env } from "cloudflare:workers";
 import { PostHog } from "posthog-node";
 
-console.log(
-  "PostHog initialized with token:",
-  env.POSTHOG_TOKEN ?? "phc_notoken",
+// https://posthog.com/docs/product-analytics/troubleshooting#is-it-ok-for-my-api-key-to-be-exposed-and-public
+// TL;DR: Yes, it's ok to expose the token
+export const posthog = new PostHog(
+  "phc_UPdNPQ9L5IfsRgPI6HlHiP4npmngdn05X4ftv8Gja1r",
+  {
+    host: "https://us.i.posthog.com",
+  },
 );
-export const posthog = new PostHog(env.POSTHOG_TOKEN ?? "phc_notoken", {
-  host: "https://us.i.posthog.com",
-});
