ba391b557cbb1e1ca3feb7bd09222c2a5e83e6c5
mdhruvil <dhruvil1808@gmail.com>
2025-05-24 16:58:27 +0530

    debug(ph): not sending events

--- apps/api/src/lib/posthog.ts
+++ apps/api/src/lib/posthog.ts
@@ -1,6 +1,10 @@
 import { env } from "cloudflare:workers";
 import { PostHog } from "posthog-node";
 
+console.log(
+  "PostHog initialized with token:",
+  env.POSTHOG_TOKEN ?? "phc_notoken",
+);
 export const posthog = new PostHog(env.POSTHOG_TOKEN ?? "phc_notoken", {
   host: "https://us.i.posthog.com",
 });
