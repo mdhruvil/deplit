8483870f880f8b1e403390a32af16ab0a79bcef1
mdhruvil <dhruvil1808@gmail.com>
2025-05-24 16:40:48 +0530

    fix(api): use dummy ph token for build time

--- apps/api/src/lib/posthog.ts
+++ apps/api/src/lib/posthog.ts
@@ -1,6 +1,6 @@
 import { env } from "cloudflare:workers";
 import { PostHog } from "posthog-node";
 
-export const posthog = new PostHog(env.POSTHOG_TOKEN, {
+export const posthog = new PostHog(env.POSTHOG_TOKEN ?? "phc_notoken", {
   host: "https://us.i.posthog.com",
 });
